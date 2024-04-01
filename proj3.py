71337e9fd1df9ad185d6b093dda54fc06d1eb5a1
badik2001 <badik20012@gmail.com>
2024-04-02 00:04:01 +0400

    i think, i don't fix bags, but only add they(

--- proj3.py
+++ proj3.py
@@ -1,43 +1,43 @@
 import random
 
 
+
 def generate_password():
     digits = list("0123456789")
     lowercase_letters = list("abcdefghijklmnopqrstuvwxyz")
     uppercase_letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     punctuation = list("!#$%&*+-=?@^_")
     chair = []
-    print(lowercase_letters)
 
-    numbers = input("Включать цифры?; д = да, н = нет")
+    numbers = input("Включать цифры?; д = да, н = нет ")
     if numbers == "д":
         chair += digits
     elif numbers == "н":
         print("Пароль будет менее надёжным")
     else:
         print('Нужно было вводить либо "д", либо "н", но теперь поздно')
-    low_letters = input("Добавить прописные буквы?; д = да, н = нет")
+    low_letters = input("Добавить прописные буквы?; д = да, н = нет ")
     if low_letters == "д":
         chair += lowercase_letters
     elif low_letters == "н":
         print("Пароль будет менее надёжным")
     else:
         print('Нужно было вводить либо "д", либо "н", но теперь поздно')
-    up_letters = input("Добавить заглавные буквы?; д = да, н = нет")
+    up_letters = input("Добавить заглавные буквы?; д = да, н = нет ")
     if up_letters == "д":
         chair += uppercase_letters
     elif up_letters == "н":
         print("Пароль будет менее надёжным")
     else:
         print('Нужно было вводить либо "д", либо "н", но теперь поздно')
-    symbols = input("Добавить символы?; д = да, н = нет")
+    symbols = input("Добавить символы?; д = да, н = нет ")
     if symbols == "д":
         chair += punctuation
     elif symbols == "н":
         print("Пароль будет менее надёжным")
     else:
         print('Нужно было вводить либо "д", либо "н", но теперь поздно')
-    ambiguous_symbols = input("Исключить неоднозначные символы?; д = да, н = нет")
+    ambiguous_symbols = input("Исключить неоднозначные символы?; д = да, н = нет ")
     if ambiguous_symbols == "д":
         del [i], [l], [L], [o], [O]
     elif ambiguous_symbols == "н":
@@ -46,9 +46,11 @@ def generate_password():
         print('Нужно было вводить либо "д", либо "н", но теперь поздно')
 
     cnt = int(input("Введите количество паролей"))
-    password = random.sample(chair, int(input("Введите длину пароля")))
-
-    print(*password)
+    print("Введите длину пароля")
+    for _ in range(1, cnt + 1):
+        print(cnt)
+        password = random.sample(chair, int(input()))
+        print(password)
 
 
 generate_password()
