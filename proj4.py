7379d0f2a9a135494fb97024810d8a144319b4ac
badik2001 <badik20012@gmail.com>
2024-04-06 22:35:50 +0400

    This is pre-last version of my project, I need dicide one problem, such as space

--- proj4.py
+++ proj4.py
@@ -1,41 +1,23 @@
 def encrypt_text():
     print('Hello, this program can encrypt your text')
-    direction = input('Enter direction cipher = cip, decipher = dec')
-    alfabet = input('Enter alfabet English = e, Russian = r')
     step = int(input('Enter a step of cipher(number)'))
     text = list(str(input('Enter a text')))
     new_text = []
-    if direction == 'cip':
-        if alfabet == 'e':
-            for c in text:
-                if c.islower():
-                    new_text.append(chr(ord(c) + step))
-                elif c.isupper():
-                    new_text.append(chr(ord(c) + step))
-        elif alfabet == 'r':
-            for c in text:
-                if c.islower():
-                    print(ord(c))
-                    new_text.append(chr((ord(c) + step) % 1071))
-                    
-                elif c.isupper():
-                    print(ord(c))
-                    new_text.append(chr((ord(c) + step) % 1071))
-                    print(ord(c))
-    elif direction == 'dec':
-        if alfabet == 'e':
-            for c in text:
-                if c.islower():
-                    new_text.append(chr(ord(c) + step))
-                    
-                elif c.isupper():
-                    new_text.append(chr(ord(c) + step))
-        elif alfabet == 'r':
-            for c in text:
-                if c.islower():
-                    new_text.append(chr(ord(c) + step))
-                elif c.isupper():
-                    new_text.append(chr(ord(c) + step))
-    print(''.join(new_text))
 
+    english = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    ENGLISH = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    russian = ["а","б","в","г","д","е","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
+    RUSSIAN = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
+    for i in range(len(text)):
+
+        
+        if text[i] in english:
+            new_text.append(english[(english.index(text[i]) + step) % 26])
+        elif text[i] in ENGLISH:
+            new_text.append(ENGLISH[(ENGLISH.index(text[i]) + step) % 26])
+        elif text[i] in russian:
+            new_text.append(russian[(russian.index(text[i]) + step) % 32])
+        elif text[i] in RUSSIAN:
+            new_text.append(RUSSIAN[(RUSSIAN.index(text[i]) + step) % 32])
+    print(*new_text)
 encrypt_text()
