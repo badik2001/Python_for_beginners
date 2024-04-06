8167e5348f157522b24d9e4551ccc01962b103df
badik2001 <badik20012@gmail.com>
2024-04-07 00:04:59 +0400

    This is final version of my program

--- proj4.py
+++ proj4.py
@@ -1,23 +1,31 @@
 def encrypt_text():
     print('Hello, this program can encrypt your text')
     step = int(input('Enter a step of cipher(number)'))
-    text = list(str(input('Enter a text')))
+    
+    text = input('Enter a text').split()
     new_text = []
+    new_text2 = []
 
     english = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     ENGLISH = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     russian = ["а","б","в","г","д","е","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
     RUSSIAN = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
-    for i in range(len(text)):
-
+    
+    for word in text:
         
-        if text[i] in english:
-            new_text.append(english[(english.index(text[i]) + step) % 26])
-        elif text[i] in ENGLISH:
-            new_text.append(ENGLISH[(ENGLISH.index(text[i]) + step) % 26])
-        elif text[i] in russian:
-            new_text.append(russian[(russian.index(text[i]) + step) % 32])
-        elif text[i] in RUSSIAN:
-            new_text.append(RUSSIAN[(RUSSIAN.index(text[i]) + step) % 32])
-    print(*new_text)
+        for i in range(len(word)):
+            
+            
+            if word[i] in english:
+                new_text.append(english[(english.index(word[i]) + step) % 26])
+                
+            elif word[i] in ENGLISH:
+                new_text.append(ENGLISH[(ENGLISH.index(word[i]) + step) % 26])
+            elif word[i] in russian:
+                new_text.append(russian[(russian.index(word[i]) + step) % 32])
+            elif word[i] in RUSSIAN:
+                new_text.append(RUSSIAN[(RUSSIAN.index(word[i]) + step) % 32])
+        new_text2.append(''.join(new_text))
+        new_text = []
+    print(*new_text2)
 encrypt_text()
